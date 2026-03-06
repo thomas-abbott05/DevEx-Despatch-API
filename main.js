3f6c3fd0b7d7a23d0b77f4a59ba9887a9cd759ea
Thomas Abbott <thomas.abbott1@students.mq.edu.au>
2026-03-06 12:49:06 +1100

    Add test route to API

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const swaggerUi = require('swagger-ui-express')
+const path = require('path')
 const swaggerSpecJSON = require('./swagger.json')
 
 const app = express()
-const port = 3000
+const PORT = 3000
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Set up Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecJSON))
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Despatch API web server started on port: ${port}`)
+app.listen(PORT, () => {
+  console.log(`Despatch API web server started on port: ${PORT}`)
 })
