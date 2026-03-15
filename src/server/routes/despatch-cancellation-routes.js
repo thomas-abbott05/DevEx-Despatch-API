000830b0f2068b7b694f1c3b1adc669876a0e597
Angela Liu <angelaliu@Angelas-Air.modem>
2026-03-15 18:38:02 +1100

    Implemented despatch/cancel/fulfilment route, wrote unit tests for helper file and main file and updated despatch-cancellation-routes to include the two new POST and GET routes

--- src/server/routes/despatch-cancellation-routes.js
+++ src/server/routes/despatch-cancellation-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { buildCancelRequestMetadata, buildCancelRetrievalMetadata } = require('../despatch/despatch-cancel-order-request-helper');
 const { cancelDespatchAdvice, getCancellation } = require('../despatch/despatch-cancel-order');
+const { buildFulfilmentCancelRequestMetadata, buildFulfilmentCancelRetrievalMetadata } = require('../despatch/despatch-cancel-fulfilment-helper');
+const { cancelDespatchWithFulfilment, getFulfilmentCancellation } = require('../despatch/despatch-cancel-fulfilment');
 const router = express.Router();
 
 // POST /api/v1/despatch/cancel/order
@@ -19,12 +21,39 @@ router.post('/order', async (req, res) => {
 // GET /api/v1/despatch/cancel/order
 router.get('/order', async (req, res) => {
   const apiKey = req.apiKey;
+
   try {
     const metadata = buildCancelRetrievalMetadata(req);
     const result = await getCancellation(apiKey, metadata);
     res.status(200).send(result);
   } catch (error) {
     return res.status(error.statusCode || 500).send({errors: [error.message]});
+  }
+});
+
+// POST /api/v1/despatch/cancel/fulfilment
+router.post('/fulfilment', async (req, res) => {
+  const apiKey = req.apiKey;
+
+  try {
+    const metadata = buildFulfilmentCancelRequestMetadata(req);
+    const result = await cancelDespatchWithFulfilment(apiKey, metadata);
+    res.status(200).send(result);
+  } catch (error) {
+    return res.status(error.statusCode || 500).send({ errors: [error.message] });
+  }
+});
+
+// GET /api/v1/despatch/cancel/fulfilment
+router.get('/fulfilment', async (req, res) => {
+  const apiKey = req.apiKey;
+  
+  try {
+    const metadata = buildFulfilmentCancelRetrievalMetadata(req);
+    const result = await getFulfilmentCancellation(apiKey, metadata);
+    res.status(200).send(result);
+  } catch (error) {
+    return res.status(error.statusCode || 500).send({ errors: [error.message] });
   }
 });
 
