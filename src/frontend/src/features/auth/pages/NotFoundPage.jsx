fc0c2dcd0fdf2c566dac3b9a8a0d9553d7a23164
Thomas Abbott <thomas-abbott05@outlook.com>
2026-04-05 20:06:45 +1000

    update 404 page styling

--- src/frontend/src/features/auth/pages/NotFoundPage.jsx
+++ src/frontend/src/features/auth/pages/NotFoundPage.jsx
@@ -2,21 +2,28 @@ import { Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card'
 import './styles/NotFoundPage.css'
+import MeshGradientBackground from '../components/MeshGradientBackground'
 
 export default function NotFoundPage() {
   return (
-    <main className="not-found-screen">
-      <Card className="not-found-card">
-        <CardHeader>
-          <CardTitle>404</CardTitle>
-          <CardDescription>The page you requested could not be found. We may have moved it, or perhaps you typed in the URL incorrectly :(</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button asChild>
-            <Link to="/">Go to home</Link>
-          </Button>
-        </CardContent>
-      </Card>
-    </main>
+    <MeshGradientBackground>
+      <main className="not-found-screen">
+        <Card className="not-found-card">
+          <CardHeader>
+            <CardTitle className="not-found-title">404</CardTitle>
+            <CardDescription className="not-found-description">
+              The page you requested could not be found. We may have moved it, or perhaps you typed in the URL incorrectly :(
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Button className="auth-main-action" style={{ marginTop: '2rem', marginBottom: '2rem' }} asChild>
+              <Link to="/">Go to home</Link>
+            </Button>
+            <img className="not-found-image" src="/img/devexlogo2.png" alt="DevEx Logo" />
+            <p style={{fontSize: '0.6rem'}}>Copyright (not really..) DevEx SENG team 26T1 😼😼😼</p>
+          </CardContent>
+        </Card>
+      </main>
+    </MeshGradientBackground>
   )
 }
